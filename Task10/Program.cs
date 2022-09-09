4e1cc02924f727bb22612ac65e51be6ce7f87e6e
PC <pkorobisyn123@gmail.com>
2022-09-09 17:11:45 +0500

    Задача 10

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿int InCenter(int number)
+{   
+    int result = ((number / 10) % 10);
+    return result;
+}
+
+Console.WriteLine("Введите трёхзначное число");
+
+int number = int.Parse(Console.ReadLine());
+int size = number.ToString().Length;
+
+if (size == 3 )
+{
+    Console.WriteLine(InCenter(number));
+}
+else
+{
+    Console.WriteLine("Вы ввели не трёхзначное число");   
+}
