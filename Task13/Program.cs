e4cce9f9865925456355611cdf73c6c426ae6527
PC <pkorobisyn123@gmail.com>
2022-09-09 17:22:55 +0500

    Добавили задачу 13

---
+++ Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введи число: ");
+string num1 = Console.ReadLine();
+if (num1.Length > 2)
+{
+    Console.WriteLine($"Третья цифра {num1[2]}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
