a3c26971958ebde68310ef5b4fc2aadf7e30b4c0
PC <pkorobisyn123@gmail.com>
2022-09-09 17:28:54 +0500

    Добавили третию задачу

---
+++ Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber == 6 || dayNumber == 7)
+{
+    Console.WriteLine("Этот день выходной");
+}
+else if (dayNumber < 1 || dayNumber > 7)
+{
+    Console.WriteLine("Такого дня недели нет");
+}
+else Console.WriteLine("Этот день не выходной");
